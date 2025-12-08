0d9c2121312838d68574071420e056f4304f340d
Ken Friis Larsen <ken@friislarsen.net>
2025-12-08 09:40:52 +0100

    Add alternative solution for day 5.

--- day05/day05.hs
+++ day05/day05.hs
@@ -11,6 +11,7 @@ import qualified Data.IntervalSet as IS
 import qualified Data.Interval as IS
 import Data.Interval ((<=..<=))
 
+
 test =  parse $ unlines [ "3-5"
                         , "10-14"
                         , "16-20"
@@ -38,12 +39,51 @@ part1 (ranges, ids) = length $ filter (\id -> any (`inRange` id) ranges) ids
 
 answer1 = part1 <$> input
 
+
+{-
+
+Idea 1: Just use the right data structure and everything will
+work. The right data structure in this case is interval sets.
+
+-}
+
 range (x,y) = fromIntegral x <=..<= fromIntegral y
 
 part2 :: Input -> Int
 part2 (ranges, _) = sum $ map ((+1) . IS.width) $ IS.toList $ IS.fromList $ map range ranges
 
 answer2 = part2 <$> input
+
+
+{-
+
+Idea 2: Don't rely on external libraries, instead work directly with
+ranges. The algorithm turns out to be quite manageable: Sort the
+ranges and merge overlapping/adjacent ranges.
+
+Curiously, it turns out to important to use foldl' and not foldr in
+`mergeRanges` for correctness and not performance. Because the input
+is sorted by start, when you see a new range `r@(lo2, up2)`, the only
+candidate it can overlap/adjacent with is the most recent merged range
+(the "rightmost so far"). And crucially: When you merge in a left
+fold, you only extend the end to the right. You do not move the start
+left.
+
+-}
+
+mergeRanges :: [Range] -> [Range]
+mergeRanges ranges = L.foldl' step [] $ L.sort ranges
+  where
+    step [] r = [r]
+    step acc@((lo1, up1) : rs) r@(lo2, up2)
+      | up1 + 1 >= lo2 = (lo1, max up1 up2) : rs -- overlap/adjacent
+      | otherwise      = r : acc
+
+part2' :: Input -> Int
+part2' (ranges, _) = sum $ map (\(lo, up) -> up - lo + 1) $ mergeRanges ranges
+
+answer2' = part2' <$> input
+
 
 main = do
   inp <- input
