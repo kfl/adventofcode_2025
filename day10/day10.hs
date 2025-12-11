086b97f72c5c19a0e7b7b20b71fb0a51f818e26a
Ken Friis Larsen <ken@friislarsen.net>
2025-12-11 21:56:38 +0100

    Day 10, part 2, with SBV

--- day10/day10.hs
+++ day10/day10.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase, ViewPatterns #-}
 module Main where
 
 import qualified Data.Char as C
@@ -16,10 +15,9 @@ import Data.Maybe (fromJust)
 import qualified Data.Vector as V
 import qualified GHC.Utils.Misc as Cheat
 
-import qualified Data.Hashable as H
-import qualified Data.OrdPSQ as Q
-import qualified Data.HashMap.Strict as HMap
-
+import Control.Monad (forM_)
+import qualified Data.SBV as S
+import Data.SBV ((.==), (.>=))
 
 
 test =  map parse [ "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
@@ -66,62 +64,31 @@ part1 input = sum [length (solve m) - 1 | m <- ms ]
           where initial = V.replicate (V.length goal) False
 answer1 = part1 <$> input
 
-type Part2Machine = (V.Vector Int, [Set Int])
 
-convert2 :: [Machine] -> [Part2Machine]
-convert2 machines = [ (V.fromList joltage, map Set.fromList buttons)
-                    | (_, buttons, joltage) <- machines]
+solveMachine :: Machine -> IO Integer
+solveMachine (_, buttons, joltage) = do
+  S.LexicographicResult res <- S.optimize S.Lexicographic $ do
 
-press2 joltage button = V.imap (\i x -> if Set.member i button then x+1 else x) joltage
+    -- variables for the number of times each button is pressed
+    xs <- S.sIntegers ["button_" ++ show i | i <- [0..length buttons - 1]]
 
-dijkstra :: (Num cost, Ord cost, Ord state)
-         => (state -> [(state, cost)])
-         -> (state -> Bool)
-         -> state
-         -> Maybe (cost, [state])
-dijkstra next found initial = loop initPathCost initPrev startFrontier
-  where
-    x `less` may = maybe True (x <) may
-    m !? state = Map.lookup state m
+    -- We cannot press a button a negative amount of times
+    S.constrain $ S.sAll (.>= 0) xs
 
-    update n c = Q.insert n c ()
+    -- Pressing the buttons must generate the correct amount of joltage
+    forM_ (zip [0..] joltage) $ \(i, j) ->
+      S.constrain $ sum [x | (x, btn) <- zip xs buttons, i `elem` btn]
+                    .== fromIntegral j
 
-    startFrontier = Q.singleton initial 0 ()
-    initPathCost  = Map.singleton initial 0
-    initPrev      = Map.empty
+    S.minimize "presses" (sum xs)
 
-    loop _ _ (Q.minView -> Nothing) = Nothing
-    loop pathCost prev (Q.minView -> Just(s, c, _, frontier))
-      | found s = Just (c, reconstruct s prev)
-      | otherwise = loop pathCost' prev' frontier'
-      where
-        relevant = [ (n, cc) | (n, sc) <- next s,
-                               let cc = c + sc,
-                               cc `less` (pathCost !? n) ]
-        (frontier', pathCost', prev') = L.foldr updateAll (frontier, pathCost, prev) relevant
-        updateAll (n, cc) (front, pathC, prev) =
-              (update n cc front, Map.insert n cc pathC, Map.insert n s prev)
+  return $ fromJust $ S.getModelValue "presses" res
 
-    reconstruct goal prev = reverse $ go [] goal
-      where go acc cur = maybe (cur:acc) (go (cur:acc)) $ Map.lookup cur prev
-
-
-
-
-part2 :: Input -> Int
-part2 input = sum [length (solve m) - 1 | m <- ms ]
-  where ms = convert2 input
-        solve (goal, buttons) = snd $ fromJust $ dijkstra next (== goal) initial
-          where initial = V.replicate (V.length goal) 0
-                valid vec = and $ V.zipWith (<=) vec goal
-                next s = [ (s', 1) | b <- buttons
-                                   , let s' = press2 s b
-                                   , valid s']
-
-answer2 = part2 <$> input
+part2 :: Input -> IO Integer
+part2 input = sum <$> mapM solveMachine input
+answer2 = part2 =<< input
 
 main = do
   inp <- input
   print $ part1 inp
-  putStrLn "Currently part2 will just spin the wheels until the OOM kills it, might as well stop it now"
-  print $ part2 inp
+  print =<< part2 inp
